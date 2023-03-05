d45a4a29906f2ce1bd21585ffaa4f635b8afd144
AlexVasilev-N <alexvasilievspb@gmail.com>
2023-03-05 11:07:07 +0300

    программа написана

--- Code/Program.cs
+++ Code/Program.cs
@@ -1,3 +1,44 @@
 ﻿
 
 
+string[] AskArray() 
+{
+    Console.Write("Введите значения через пробел: ");
+    return Console.ReadLine().Split(" ");
+}
+
+int CheckArray(string[] input, int find)
+ {
+    int count = 0;
+
+    for(int i = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= find) 
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] FindLessThan(string[] input, int find) 
+{
+    string[] output = new string[CheckArray(input, find)];
+
+    for(int i = 0, j = 0; i < input.Length; i++) 
+    {
+        if(input[i].Length <= find) 
+        {
+            output[j] = input[i];
+            j++;
+        }
+    }
+
+    return output;
+}
+
+int find = 2;
+string[] array = AskArray();
+string[] result = FindLessThan(array, find);
+Console.WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
